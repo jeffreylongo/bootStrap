fe9b797b36c452d92ecada7525c0cbd39f266211
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-19 11:12:36 -0400

    made sports and teams models

---
+++ bootStrap/bootStrap/Models/Sports.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bootStrap.Models
+{
+    public class Sports
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TeamId { get; set; }
+    }
+}
---
+++ bootStrap/bootStrap/Models/Teams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bootStrap.Models
+{
+    public class Teams
+    {
+        public int Id { get; set; }
+        public int SportId { get; set; }
+        public string Name { get; set; }
+        public int NumberOfPlayers { get; set; }
+
+    }
+}
